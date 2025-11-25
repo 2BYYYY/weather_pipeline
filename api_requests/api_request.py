b6820e8c3ba921730e16f4e27326eff1257ccfca
Toby Javelona <tobyyjavelona@gmail.com>
2025-11-25 20:23:10 +0800

    feat: established connection to postgres in docker

--- api_requests/api_request.py
+++ api_requests/api_request.py
@@ -5,17 +5,7 @@ from dotenv import load_dotenv
 import os
 import requests
 import argparse
-
-load_dotenv()
-
-parser = argparse.ArgumentParser()
-parser.add_argument("country", nargs="+", help="Input the country or city name")
-args = parser.parse_args()
-
-# build the URL
-COUNTRY = " ".join(args.country)
-API_KEY = os.getenv("WEATHER_API")
-WEATHER_URL = "http://api.weatherstack.com/current"
+import json
 
 def fetch_data():
     try:
@@ -23,11 +13,86 @@ def fetch_data():
             "access_key": API_KEY,
             "query": COUNTRY
         }
-        # Sample url: http://api.weatherstack.com/current?access_key=YOUR_ACCESS_KEY&query=New York
+        # Sample url: http://api.weatherstack.com/current?access_key=YOUR_ACCESS_KEY&query=COUNTRY
         response = requests.get(WEATHER_URL, params=params)
-        return response.json()
+        json_response = response.json()
+        foramtted_response = json.dumps(json_response, indent=2)
+        return foramtted_response
     except Exception as e:
-        return {"error": str(e)}
+        print(f"Error in fetching the data: {e}")
+        raise
+
+def mock_data():
+    data = {
+            "request": {
+                "type": "City",
+                "query": "New York, United States of America",
+                "language": "en",
+                "unit": "m"
+            },
+            "location": {
+                "name": "New York",
+                "country": "United States of America",
+                "region": "New York",
+                "lat": "40.714",
+                "lon": "-74.006",
+                "timezone_id": "America/New_York",
+                "localtime": "2025-11-25 00:52",
+                "localtime_epoch": 1764031920,
+                "utc_offset": "-5.0"
+            },
+            "current": {
+                "observation_time": "05:52 AM",
+                "temperature": 7,
+                "weather_code": 116,
+                "weather_icons": [
+                "https://cdn.worldweatheronline.com/images/wsymbols01_png_64/wsymbol_0004_black_low_cloud.png"
+                ],
+                "weather_descriptions": [
+                "Partly cloudy"
+                ],
+                "astro": {
+                "sunrise": "06:55 AM",
+                "sunset": "04:31 PM",
+                "moonrise": "11:27 AM",
+                "moonset": "09:02 PM",
+                "moon_phase": "Waxing Crescent",
+                "moon_illumination": 19
+                },
+                "air_quality": {
+                "co": "384.85",
+                "no2": "57.05",
+                "o3": "0",
+                "so2": "11.05",
+                "pm2_5": "19.15",
+                "pm10": "19.45",
+                "us-epa-index": "2",
+                "gb-defra-index": "2"
+                },
+                "wind_speed": 7,
+                "wind_degree": 219,
+                "wind_dir": "SW",
+                "pressure": 1025,
+                "precip": 0,
+                "humidity": 60,
+                "cloudcover": 75,
+                "feelslike": 5,
+                "uv_index": 0,
+                "visibility": 16,
+                "is_day": "no"
+            }
+            }
+    return data
 
 if __name__ == "__main__":
+    load_dotenv()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("country", nargs="+", help="Input the country or city name")
+    args = parser.parse_args()
+
+    # build the URL
+    COUNTRY = " ".join(args.country)
+    API_KEY = os.getenv("WEATHER_API")
+    WEATHER_URL = "http://api.weatherstack.com/current"
     print(fetch_data())
---
+++ api_requests/insert_records.py
@@ -0,0 +1,81 @@
+from api_request import mock_data
+import psycopg2
+
+def connect_to_db():
+    try:
+        conn = psycopg2.connect(
+            dbname="db",
+            user="db_user",
+            password="db_password",
+            host="localhost",
+            port=5000
+        )
+        return conn
+    except Exception as e:
+        print(f"Database connection failed: {e}")
+        raise
+
+def create_table(conn: object):
+    try:
+        cursor = conn.cursor()
+        cursor.execute("""
+            CREATE SCHEMA IF NOT EXISTS dev;
+            CREATE TABLE IF NOT EXISTS dev.raw_weather_data (
+                id SERIAL PRIMARY KEY,
+                city TEXT,
+                temperature FLOAT,
+                weather_descriptions TEXT,
+                wind_speed FLOAT,
+                time TIMESTAMP,
+                inserted_at TIMESTAMP DEFAULT NOW(),
+                utc_offset TEXT
+            );
+                       
+        """)
+        conn.commit()
+        print("table was created")
+    except Exception as e:
+        print(f"Failed to create table: {e}")
+        raise
+
+def insert_records(conn: object, data: dict):
+    try:
+        cursor = conn.cursor()
+        location = data['location']
+        weather = data['current']
+        cursor.execute("""
+            INSERT INTO dev.raw_weather_data(
+                city,
+                temperature,
+                weather_descriptions,
+                wind_speed,
+                time,
+                inserted_at,
+                utc_offset)
+                VALUES(%s, %s, %s, %s, %s, NOW(), %s)
+            """,(
+                location['name'],
+                weather['temperature'],
+                weather['weather_descriptions'][0],
+                weather['wind_speed'],
+                location['localtime'],
+                location['utc_offset']
+
+            ))
+        conn.commit()
+        print("Data inserted")
+    except Exception as e:
+        print(f"Data was unsuccessful: {e}")
+        
+if __name__ == "__main__":
+    try:
+        data = mock_data()
+        conn = connect_to_db()
+        create_table(conn)
+        insert_records(conn, data)
+    except Exception as e:
+        print(f"An error occured during execution {e}")
+    finally:
+        if "conn" in locals():
+            conn.close()
+            print("Database closed")
